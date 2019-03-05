952474329370a4244dbff501a70cf810ae091b31
HarishSharma1991 <47175133+HarishSharma1991@users.noreply.github.com>
2019-03-05 12:30:50 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+   private String Name ;
+   private String MailId;
+   private int Id ;
+   private int Doj ;
+
+
+
+   private Employee (String name, String MailID){
+     this.Name = name;
+     this.MailId = MailID;
+     Name Name = new Name("Harish");
+     MailId mailId = new MailId("harish.sharma@hashmapinc.com");
+
+
+   }
+}
