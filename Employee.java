b6e19dac9e53673233bccec8946adc515262551b
HarishSharma1991 <47175133+HarishSharma1991@users.noreply.github.com>
2019-03-05 13:20:57 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -3,14 +3,19 @@ public class Employee {
    private String MailId;
    private int Id ;
    private int Doj ;
+   map<String, int>;
 
 
 
-   private Employee (String name, String MailID){
+   private Employee (String name, String MailID, int Id){
      this.Name = name;
      this.MailId = MailID;
+     this.Id = Id;
      Name Name = new Name("Harish");
      MailId mailId = new MailId("harish.sharma@hashmapinc.com");
+     map <String, int> hm = new hashmp<"Probation", 0>;
+     map <String, int> hm = new hashmp<"Permanent", 25>;
+     
 
 
    }
